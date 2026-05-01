e84d506f3ac64fcf7925b4c32ffe5f8af45d6721
Janavi1101 <janavihgowda.brigade@gmail.com>
2026-05-01 20:41:54 +0530

    complete file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", 
+  base: "/mern-application/", 
   build: {
-    outDir: "dist", 
+    outDir: "dist",
   },
 });
